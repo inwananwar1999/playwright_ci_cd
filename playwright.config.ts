4d1520b4e6620ca7d2f2d9397346291e483f92b9
inwananwar1999 <inwananwar9b@gmail.com>
2026-08-21 09:36:31 +0000

    Update .yaml test-result

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,14 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'],['html'],['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
+  use: {trace: 'on-first-retry'},
   /* Configure projects for major browsers */
   projects: [
     {
